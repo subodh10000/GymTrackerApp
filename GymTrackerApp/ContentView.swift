fe530c0239572c78ab7f444921c746785a642916
subodh10000 <kathayatsubodh20@gmail.com>
2025-04-16 15:59:20 -0400

    Initial Commit

---
+++ GymTrackerApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GymTrackerApp
+//
+//  Created by Subodh Kathayat on 4/16/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
